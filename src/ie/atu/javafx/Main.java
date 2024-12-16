bee195416c08cd8e710da51c9e4bd4f9235c79e7
G00416271 <114396648+G00416271@users.noreply.github.com>
2024-12-16 18:37:56 +0000

    javafx  button / lamda expressions

--- src/ie/atu/javafx/Main.java
+++ src/ie/atu/javafx/Main.java
@@ -1,36 +1,37 @@
 package ie.atu.javafx;
 import javafx.application.Application;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+public class Main extends Application  {
 
-public class Main extends Application{
-    @Override
-    public void start(Stage pramaryStage){
-        System.out.println("Javafx Application Started");
-    }
-public class HelloJavaFX extends Application{
-    @Override
-    public void start(Stage primaryStage){
-        Label welcomeLabel = new Label("Welcome to javafx");
-        welcomeLabel.setStyle("-fx-font-size: 20px;");
-
-        StackPane root = new StackPane();
-        root.getChildren().add(welcomeLabel);
-
-        Scene scene = new Scene(root, 300, 200);
-
-        primaryStage.setTitle("Hello JavaFX");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-}
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
 
+        // Create four buttons
+        Button button1 = new Button("Button 1"); button1.setOnAction(e -> System.out.println("hi, f*ck you <3"));
+        Button button2 = new Button("Button 2");
+        Button button3 = new Button("Button 3");
+        Button button4 = new Button("Button 4");
+
+        // Create a layout and add the buttons
+        VBox layout = new VBox(10); // VBox with 10px spacing
+        layout.getChildren().addAll(button1, button2, button3, button4);
+
+        
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
